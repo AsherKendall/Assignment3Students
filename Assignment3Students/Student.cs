80000c8e312bba58d7b39f57a40803fec111bc15
asher <ashtonday09@gmail.com>
2023-02-26 13:35:46 -0600

    Started student class

--- Assignment3Students/Student.cs
+++ Assignment3Students/Student.cs
@@ -8,6 +8,36 @@ namespace Assignment3Students
 {
     public class Student
     {
+        private readonly int _id;
+        private string _name;
+        private string _major;
+        private string _startDate;
+        private string _anticipatedGraduationDate;
+        private string _stateProvinceTerritory;
+        private string _country;
+        private string _email;
+        private int _phoneNumber;
+        private string _mailingAddress;
+
+        public int PhoneNumber { get; set; }
+
+
+        #region Constructors
+        public Student(int id)
+        {
+            this._id = id;
+        }
+        public Student(int id,string name,string major,string country,string email, int phoneNumber, string mailingAddress)
+        {
+            this._id= id;
+            this._name= name;
+            this._major= major;
+            this._country= country;
+            this._email= email;
+            this._phoneNumber = phoneNumber;
+            this._mailingAddress= mailingAddress;
+        }
+        #endregion
 
     }
 }
