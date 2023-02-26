fdb53664cf1cb1afd3fda30b6570896fe364fc0f
asher <ashtonday09@gmail.com>
2023-02-26 14:34:00 -0600

    more changes

--- Assignment3Students/Student.cs
+++ Assignment3Students/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,54 @@ namespace Assignment3Students
 {
     public class Student
     {
-        private readonly int _id;
+
+
+
+        #region Properties-Fields
+        private List<string> _history;
+
+        private void AddHistory(string property, string from, string to)
+        {
+            _history.Add(property + ": " + from + "=> " + to);
+        }
+
+        public readonly int _id;
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
         private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                AddHistory("Name", _name, value);
+                _name = value;
+            }
+        }
+
         private string _major;
+        public string Major
+        {
+            get
+            {
+                return _major;
+            }
+            set
+            {
+                AddHistory("Major", _name, value);
+                _major = value;
+            }
+        }
+
         private string _startDate;
         private string _anticipatedGraduationDate;
         private string _stateProvinceTerritory;
@@ -18,24 +64,38 @@ namespace Assignment3Students
         private string _email;
         private int _phoneNumber;
         private string _mailingAddress;
+        private bool _isPrivate = false;
 
-        public int PhoneNumber { get; set; }
+        #endregion
 
+
+
+
+
+
+
+        #region Methods
+
+
+
+
+        #endregion
 
         #region Constructors
-        public Student(int id)
+        public Student(int id,string name)
         {
-            this._id = id;
+            _id = id;
+            _name = name;
         }
         public Student(int id,string name,string major,string country,string email, int phoneNumber, string mailingAddress)
         {
-            this._id= id;
-            this._name= name;
-            this._major= major;
-            this._country= country;
-            this._email= email;
-            this._phoneNumber = phoneNumber;
-            this._mailingAddress= mailingAddress;
+            _id = id;
+            _name= name;
+            _major = major;
+            _country = country;
+            _email = email;
+            _phoneNumber = phoneNumber;
+            _mailingAddress = mailingAddress;
         }
         #endregion
 
